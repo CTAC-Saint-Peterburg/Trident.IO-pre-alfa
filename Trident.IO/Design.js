78143f3b0e4f3facabe412d3aaddb0c699822757
Стас <seleznev-00@mail.ru>
2021-10-06 19:04:32 +0300

    feat: css

--- Trident.IO/Design.js
+++ Trident.IO/Design.js
@@ -1,5 +1,12 @@
 ﻿function PlayGame() {
     document.getElementById("canvas").style.visibility = "visible";
-    document.getElementById("greeting").style.visibility = "hidden";
+    document.getElementById("header").style.visibility = "hidden";
     document.getElementById("main").style.visibility = "hidden";
+    document.getElementById("footer").style.visibility = "hidden";
+
+}
+function shopOptions() {
+    let colors = ["red", "green", "blue", "orange", "pink"];
+    let index = 0;
+    document.getElementById('');
 }
